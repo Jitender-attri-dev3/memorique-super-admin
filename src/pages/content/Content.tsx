1ae7d8ede3f32acf91fa2378b90307a484179292
Dev3 <jitenderattri3@gmail.com>
2025-05-21 23:04:33 +0530

    border color changes

--- src/pages/content/Content.tsx
+++ src/pages/content/Content.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Content = () => {
-  return <div className="p-3">Pricing & Content</div>;
+  return (
+    <div className="p-3">
+      <h5 className="border-b pb-2"> Pricing & Content</h5>
+    </div>
+  );
 };
 
 export default Content;
