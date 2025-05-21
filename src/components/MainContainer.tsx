af2ba0555d7248923f6c75b2281973ede444a079
Dev3 <jitenderattri3@gmail.com>
2025-05-22 00:25:58 +0530

    super admin dashboard main styling

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -7,15 +7,16 @@ import { Outlet } from "react-router";
 const MainContainer = () => {
   return (
     <div className="flex h-screen">
-      <div className=" rounded bg-white p-2 shadow">
+      <div className="p-2">
         <div className="flex items-center h-[100%] flex-col justify-center gap-2">
-          <button className="p-2 bg-gray-100 rounded-2xl">
+          <button className="p-2 rounded-2xl flex items-center gap-2">
             <FcHome className="text-2xl" />
+            Home
           </button>
-          <button className="p-2 bg-gray-100 rounded-2xl">
+          <button className="p-2 rounded-2xl">
             <FcParallelTasks className="text-2xl" />
           </button>
-          <button className="p-2 bg-gray-100 rounded-2xl">
+          <button className="p-2 rounded-2xl">
             <FcServices className="text-2xl" />
           </button>
         </div>
--- src/pages/content/Content.tsx
+++ src/pages/content/Content.tsx
@@ -1,9 +1,22 @@
-import React from "react";
-
 const Content = () => {
   return (
-    <div className="p-3">
-      <h5 className="border-b pb-2"> Pricing & Content</h5>
+    <div
+      style={{
+        height: "calc(100vh - calc(var(--spacing) * 6))",
+      }}
+      className=" bg-white m-3 border  h-screen rounded-2xl flex flex-col"
+    >
+      <div className="border-b p-4">
+        <div className="flex justify-between items-center">
+          <h4 className="text-lg">Pricing & Content</h4>
+          <button className="bg-black text-white py-2 px-4 rounded text-sm">
+            Add new Price
+          </button>
+        </div>
+      </div>
+      <div className="grow"></div>
+
+      <div>{/* bottom content */}</div>
     </div>
   );
 };
